98987f46d24df218234085937a4f51532b2d2be8
arhipochki <36302108+arhipochki@users.noreply.github.com>
2022-04-19 15:18:15 +0300

    Add files via upload

--- SecondProgLab/Form1.Designer.cs
+++ SecondProgLab/Form1.Designer.cs
@@ -68,6 +68,10 @@
             this.removeGroupBtn = new System.Windows.Forms.Button();
             this.changeGroupNameBtn = new System.Windows.Forms.Button();
             this.changeTeacherSubjectsBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.studentsTable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.suggestionTeachersTable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.allTeachersTable)).BeginInit();
@@ -424,11 +428,54 @@
             this.changeTeacherSubjectsBtn.UseVisualStyleBackColor = true;
             this.changeTeacherSubjectsBtn.Click += new System.EventHandler(this.changeTeacherSubjectsBtn_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(150, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 20);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Список студентов:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(570, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Список всех предметов:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(1090, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(237, 20);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Список всех преподавателей:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(1090, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(317, 20);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Список предложенных преподавателей:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1582, 737);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.changeTeacherSubjectsBtn);
             this.Controls.Add(this.changeGroupNameBtn);
             this.Controls.Add(this.removeGroupBtn);
@@ -502,6 +549,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn ExamGrade;
         private System.Windows.Forms.DataGridViewTextBoxColumn TotalGrade;
         private System.Windows.Forms.DataGridViewTextBoxColumn StudentRatinColumn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
